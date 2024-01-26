bca2bc8c87ae6d7e8eef91b4b5e0e45b4a5ddfdc
SecretDwarf <briggsjjacob@gmail.com>
2024-01-25 18:20:44 -0700

    New resume

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ResumeAnimation from './components/ResumeAnimation';
 import ResumeAnimation2 from './components/ResumeAnimation2';
 import logo from './assets/JB.png';
-import resume from './assets/JacobBriggsResumePDF.pdf';
+import resume from './assets/JacobBriggsResume.pdf';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
 
